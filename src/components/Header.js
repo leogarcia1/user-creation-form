c783e03487a022a0d1e9046620dccfade8d45468
leogarcia1 <61811562+leogarcia1@users.noreply.github.com>
2022-09-21 18:29:53 -0500

    indented header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,19 +6,19 @@ export default function Header() {
   return (
     <div className='header'>
 
-    <img src={logo} alt="Logo" className='logo'/>
+      <img src={logo} alt="Logo" className='logo'/>
 
-    <div>
-        <span className='title'>FETCH REWARDS: </span>
-        <span className='subtitle'>Frontend Take-Home Exercise</span>
-    </div>
+      <div>
+          <span className='title'>FETCH REWARDS: </span>
+          <span className='subtitle'>Frontend Take-Home Exercise</span>
+      </div>
 
-    <div className='info-container'>
-        <span>Leonardo Garcia Calderon</span>
-        <span>336.558.7391</span>
-        <span>gc13leo@gmail.com</span>
-        <a href="https://www.linkedin.com/in/leogarciacalderon/" rel="noreferrer">LinkedIn</a>
-    </div>
+      <div className='info-container'>
+          <span>Leonardo Garcia Calderon</span>
+          <span>336.558.7391</span>
+          <span>gc13leo@gmail.com</span>
+          <a href="https://www.linkedin.com/in/leogarciacalderon/" rel="noreferrer">LinkedIn</a>
+      </div>
 
     </div>
   );
