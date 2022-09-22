c9268aa8dacae7dfe7dc7f8a1fc6a4a7d80f3da0
leogarcia1 <61811562+leogarcia1@users.noreply.github.com>
2022-09-21 20:00:14 -0500

    finshed project

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,24 +4,55 @@ import "react-widgets/styles.css";
 import DropdownList from "react-widgets/DropdownList";
 import './Form.css';
 
-const baseURL = "https://frontend-take-home.fetchrewards.com/form";
+const baseURL = "https://frontend-take-home.fetchrewards.com/form";             //endpoint for GET and POST requests
 
 export default function Form() {
-  const [form, setForm] = React.useState(null);
+  const [form, setForm] = React.useState(null);     // GET response
+  const [post, setPost] = React.useState(null);     // POST response
 
+
+  // states to hold current values of the form
   const[name, setName] = React.useState("");
   const[email, setEmail] = React.useState("");
   const[password, setPassword] = React.useState("");
   const[occupation, setOccupation] = React.useState(null);
   const[state, setState] = React.useState(null);
 
+  // GET request sent to endpoint - returns occupation and state
   React.useEffect(() => {
     axios.get(baseURL).then((response) => {
       setForm(response.data);
     });
   }, []);
 
-  console.log(form)
+  // This function is called when the submit button is pressed.
+  function submit() {
+    // if form has not been complete
+    if(name === "" || email === "" || password === "" || occupation === null || state === null) {
+        alert("Please complete the entire form before submitting.");
+    }
+
+    // if form is complete 
+    else {
+        // POST request sent to endpoint
+        axios
+            .post(baseURL, {
+                name: name,
+                email: email,
+                password: password,
+                occupation: occupation,
+                state: state
+        })
+        .then((response) => {
+          setPost(response.data);
+        });
+
+        alert("Your form has been submitted. The page will refresh now.");
+
+        window.location.reload(false); // reloads page
+    }
+
+  }
 
   return(
     <div className="form-container">
@@ -46,7 +77,8 @@ export default function Form() {
             <DropdownList
             className="dropdown-list"
             data={form === null ? [] : form.occupations}
-            onChange={e => setOccupation(e.target.value)}
+            onChange={value => setOccupation(value)}
+            value={occupation}
             />
         </div>
 
@@ -55,9 +87,14 @@ export default function Form() {
             <DropdownList
             className="dropdown-list"
             data={form === null ? [] : form.states.map(obj => obj.name)}
-            onChange={e => setState(e.target.value)}
+            onChange={value => setState(value)}
+            value={state}
             />
         </div>
+
+        <button onClick={submit}>
+            SUBMIT
+        </button>
 
     </div>
   );
